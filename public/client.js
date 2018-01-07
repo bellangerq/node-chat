0354aa7ac839ef77939da2edbc72f5643d8aa43c
Quentin Bellanger <hello@quentin-bellanger.com>
2018-01-07 17:11:05 +0100

    Add production url to socket 10

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('https://dry-badlands-98503.herokuapp.com/');
 
 var messageForm = document.querySelector('#message-form');
 var message = document.querySelector("input[name='message']");
